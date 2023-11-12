cafa5490fe5194d42d4d149e53091ff252dbd119
didikstl <125580027+didikstl@users.noreply.github.com>
2023-11-12 19:45:53 +0200

    HW-5

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,4 +1,27 @@
 from django.http.response import HttpResponse
+from app.currency.models import Rate, ContactUs
 
 def hello(request):
-    return HttpResponse('HelooWord!!!')
+    return HttpResponse('HeloWord!!!')
+
+def rate_list(request):
+    results = []
+    rates = Rate.objects.all()
+
+    for rate in rates:
+        results.append(
+            f'ID {rate.id}, buy: {rate.buy}. sell: {rate.sell}, type: {rate.type},'
+            f' source: {rate.source}, created: {rate.created} <br>'
+        )
+    return HttpResponse(str(results))
+
+
+def message(request):
+    results = []
+    infos = ContactUs.objects.all()
+
+    for info in infos:
+        results.append(
+            f'ID {info.id}, email: {info.email}, subject: {info.subject}, type: {info.message} <br>'
+        )
+    return HttpResponse(str(results))
