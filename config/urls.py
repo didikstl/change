ea7425143cbbb2c5e79a9551084e879528c90538
didikstl <125580027+didikstl@users.noreply.github.com>
2023-11-20 14:26:32 +0200

    HW-6

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from app.currency.views import hello, rate_list, message
+from app.currency.views import hello, rate_list, message, tets_templates
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/word', hello),
     path('rate/list', rate_list),
     path('contactus/list', message),
+    path('template/', tets_templates),
 
 ]
